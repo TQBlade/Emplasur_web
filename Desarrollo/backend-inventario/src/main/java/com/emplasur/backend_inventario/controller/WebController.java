f9380908d7bdfa61ccb60caf7a50ece20dad3b18
owen854 <145795287+owen854@users.noreply.github.com>
2025-11-24 23:25:19 -0500

    connection back-front

---
+++ Desarrollo/backend-inventario/src/main/java/com/emplasur/backend_inventario/controller/WebController.java
@@ -0,0 +1,32 @@
+package com.emplasur.backend_inventario.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller // OJO: Usa @Controller, NO @RestController
+public class WebController {
+
+    // Cuando entren a "localhost:8080/", mostramos el home
+    @GetMapping("/")
+    public String mostrarHome() {
+        return "home"; // Busca "home.html" en templates
+    }
+
+    // Cuando entren a "localhost:8080/login"
+    @GetMapping("/login")
+    public String mostrarLogin() {
+        return "login"; // Busca "login.html" en templates
+    }
+
+    // Cuando entren a "localhost:8080/inventario"
+    @GetMapping("/inventario")
+    public String mostrarInventario() {
+        return "inventario"; // Busca "inventario.html" en templates
+    }
+    
+    // Agrega aquí mappings para "nosotros", "contactanos", etc.
+    @GetMapping("/nosotros")
+    public String mostrarNosotros() {
+        return "nosotros";
+    }
+}
